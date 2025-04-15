78b302a22bdb4dc153887577077bf3df3ff96b6e
juancruzCT <juancruzctclases12@gmail.com>
2025-04-15 18:00:07 -0300

    tpintegprog3

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from zeep import Client
+
+app = Flask(__name__)
+
+# Cliente SOAP apuntando al WSDL de la API
+client = Client('https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL')
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = None
+    if request.method == 'POST':
+        number = request.form.get('number')
+        if number.isdigit():
+            result = client.service.NumberToWords(ubiNum=int(number))
+        else:
+            result = "Por favor, ingresa un número válido."
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
